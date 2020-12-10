bb112ab53f36d0dde518dfae408b347344b1d61b
rodey <rodey.luo@cignacmb.com>
2020-12-10 21:48:57 +0800

    add watchOption
    添加watch选项：{@Object object} watchOption

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
  */
 'use strict';
 
-const
-    gulp   = require('gulp');
-
 const create = require('./bin/create');
 const task = require('./bin/task').task;
 const start = require('./bin/task').start;
@@ -14,10 +11,14 @@ const remove = require('./bin/add').remove;
 const version = require('./bin/version').displayVersion;
 const versions = require('./bin/version').displayDescVersion;
 const Gupack = require('./lib/config/gupack');
-module.exports = { create, task, start, publish, remove, version, versions, Gupack, instance: Gupack };
-// module.exports.Gupack = function(config){
-//     let gp = new Gupack(config, gulp);
-//     gp.run();
-// };
-
-
+module.exports = {
+  create,
+  task,
+  start,
+  publish,
+  remove,
+  version,
+  versions,
+  Gupack,
+  instance: Gupack
+};
